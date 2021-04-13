03b33b3bf6429aa57a7342065bd62ed53b0e3339
Natalia Ramirez Roman <270037658@bun2-206-11.acg.ac.nz>
2021-04-13 16:10:52 +1200

    list page ready with menu

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ const navIcon = document.getElementById('navIcon');
 const navPanel = document.getElementById('navPanel');
 
 
-navIcon.addEventListener('click', () => {
-    if(navPanel.style.display === 'none'){
-        navPanel.style.display = 'block';
-    } else {
-        navPanel.style.display = 'none';
-    }
-})
+// navIcon.addEventListener('click', () => {
+//     if(navPanel.style.display === 'none'){
+//         navPanel.style.display = 'block';
+//     } else {
+//         navPanel.style.display = 'none';
+//     }
+// })
 
 // CANCEL BUTTON
 function goBack() {
